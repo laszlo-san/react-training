32e8dc51de257b94e7777ad01e9957f1357e5cc9
Szanto Laszlo <laszlo.szanto@gmail.com>
2019-07-24 14:35:30 +0200

    adding new thingis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Laci", age: 32 },
+      { name: "Dóri", age: 28 },
+      { name: "Dani", age: 0 }
+    ]
+  };
+
+  swtichNameHandler = ()=>{
+    // console.log("was clicked!");
+    this.setState({})
+  };
+
   render() {
     return (
-      <div>
-        Loooool! 
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.swtichNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Cooking
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
