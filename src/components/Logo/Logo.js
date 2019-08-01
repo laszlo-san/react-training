f19267d74163648e8ae186bb1ce3474747a04b03
Szanto Laszlo <11998934+laszlo-san@users.noreply.github.com>
2019-08-01 13:37:57 +0200

    working on the toolbar and the sidedrawer both mobile and desktop view

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import ''
+import burgerLogo from '../../assets/burger-logo.png';
+import classes from './Logo.module.css';
 
 const logo = props => (
-  <div>
-    <img src={}/>
+  <div className={classes.Logo} style={{height: props.height}}>
+    <img src={burgerLogo} alt="MyBurger"/>
   </div>
 );
 
